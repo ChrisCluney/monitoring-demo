7e6e3715eb8f9f12bd4d05d6d874f12b56e66f62
ChrisCluney <ccluney@gmail.com>
2022-02-24 13:11:15 -0600

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const path = require('path')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c9087ee406c64e57bc8c0287ad66fa71',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.get('/', (req, res) => {
